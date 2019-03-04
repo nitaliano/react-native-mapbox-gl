53a642a02e68efaff0b5f176ceb9dce304f5bb8e
Oliver Nitzschke <o.nitzschke@droidsolutions.de>
2019-03-04 23:24:35 +0100

    fixed #1524

--- javascript/utils/index.js
+++ javascript/utils/index.js
@@ -69,9 +69,11 @@ export function cloneReactChildrenWithProps(children, propsToAdd = {}) {
 
   if (!Array.isArray(children)) {
     foundChildren = [children];
+  } else {
+    foundChildren = children
   }
 
-  const filteredChildren = foundChildren.filter(child => child != null); // filter out falsy children, since some can be null
+  const filteredChildren = foundChildren.filter(child => !!child); // filter out falsy children, since some can be null
   return React.Children.map(filteredChildren, child =>
     React.cloneElement(child, propsToAdd),
   );
